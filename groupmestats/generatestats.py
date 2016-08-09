3404c25dd0a6f02f2f371de120ac42b6f81c8321
Kyle Teske <kjteske@gmail.com>
2016-08-08 22:53:05 -0500

    Add options to specify which stat(s) to generate

--- groupmestats/generatestats.py
+++ groupmestats/generatestats.py
@@ -9,11 +9,21 @@ def gstat_stats():
     parser = argparse.ArgumentParser(description="Generates stats for a group")
     parser.add_argument("-g", "--group", dest="group_name", required=True,
                         help="Group to generate stats for.")
+    parser.add_argument("-s", "--stat", dest="stats", default=[],
+                        action="append",
+                        help=("Name of stat to generate. This may be specified"
+                              " more than once. Choices: %s "
+                              % ", ".join(all_statistics.keys())))
+    parser.add_argument("--all-stats", action="store_true", default=False,
+                        help="Generate all possible stats.")
     args = parser.parse_args()
+    stats = [stat_class() for name, stat_class in all_statistics.items()
+                                    if args.all_stats or name in args.stats]
+    if not stats:
+        parser.print_help()
+        raise RuntimeError("Must specify a valid --stat or use --all-stats")
 
     (group, messages) = GroupSerializer.load(args.group_name)
-
-    stats = [stat_class() for name, stat_class in all_statistics.items()]
     for stat in stats:
         stat.calculate(group, messages)
     for stat in stats:
